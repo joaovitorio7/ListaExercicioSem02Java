93b6d0f7ca8a16403e1466377cefdf7c2e8e02d1
Joao <joaoprodutor92@gmail.com>
2023-10-03 20:54:18 -0300

    inicializa com os valores que recebeu / cria construtor

--- src/main/java/ex01/Data.java
+++ src/main/java/ex01/Data.java
@@ -4,4 +4,10 @@ public class Data {
     private int ano;
     private int mes;
     private int dia;
+
+    public Data(int dia, int mes, int ano) {
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+    }
 }
