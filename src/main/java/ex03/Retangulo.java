f7447542e81f8aab5814b71a808244cacb6faef7
Joao <joaoprodutor92@gmail.com>
2023-10-03 21:21:23 -0300

    inicia ex03

---
+++ src/main/java/ex03/Retangulo.java
@@ -0,0 +1,23 @@
+package ex03;
+
+public class Retangulo {
+    private double comprimento = 1;
+
+    private double largura = 1;
+
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+}
