8cc7ef5c6da4aff349f17f93577e82a7fab6d6f9
Joao <joaoprodutor92@gmail.com>
2023-10-03 21:05:51 -0300

    finaliza ex01

--- src/main/java/ex01/TestaData.java
+++ src/main/java/ex01/TestaData.java
@@ -1,4 +1,24 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class TestaData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um ano:");
+        int ano = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite um mês:");
+        int mes = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite um dia:");
+        int dia = Integer.parseInt(scanner.nextLine());
+
+        Data novaData = new Data(dia, mes, ano);
+
+        System.out.println(novaData.exibirData());
+
+
+    }
 }
